8ae2226dfc01cdc0db20b176df973ca1b918ac14
gsmunizml <gabriel.smuniz@mercadolivre.com>
2022-09-22 19:26:24 -0300

    feat: added main class with activity1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Array de cidades:
+        String[] cidades = {""};
+
+        // Matriz de temperaturas:
+        int[][] temperaturas = {{}};
+    }
+}
